f44ba684177eca75cd0787196df6e889a4998662
gabrielgaddiel@gmail.com <igabrieloficial7@gmail.com>
2025-11-14 23:35:26 -0500

    feat(lesson-04): Create the ProductList component

---
+++ src/app/product-list/product-list.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.css',
+})
+export class ProductList {
+    products = [
+    { id: 1, name: 'Product 1', price: 10 },
+    { id: 2, name: 'Product 2', price: 20 },
+    { id: 3, name: 'Product 3', price: 30 },
+  ];
+}
+
