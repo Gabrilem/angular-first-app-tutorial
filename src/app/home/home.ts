a4bb090eb2a6db38c3f3993c274f90181355edf6
gabrielgaddiel@gmail.com <igabrieloficial7@gmail.com>
2025-11-17 19:44:13 -0500

    Lección 5: navegación con rutas y componente product-detail

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductList } from '../product-list/product-list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
+  imports: [CommonModule, ProductList],
+  template: `
+    <h1>Bienvenido a nuestra tienda</h1>
+    <app-product-list></app-product-list>
+  `
 })
-export class Home {
-
-}
+export class Home {}
